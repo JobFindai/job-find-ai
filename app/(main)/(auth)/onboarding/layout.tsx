edf6db4944f1d06d5cc445e01f6fa7b180bc0ddf
egentinnamdi <egentinnamdi10@gmail.com>
2026-03-04 18:26:30 +0100

    feat(onboarding): add Suspense with Loader fallback to layout
    Wrap the onboarding layout in a Suspense boundary to handle loading states gracefully while child components are being fetched or rendered.

--- app/(main)/(auth)/onboarding/layout.tsx
+++ app/(main)/(auth)/onboarding/layout.tsx
@@ -1,3 +1,10 @@
+import Loader from "@/components/Loader";
+import { Suspense } from "react";
+
 export default function layout({ children }: { children: React.ReactNode }) {
-  return <div className=" py-10 pb-20 w-full">{children}</div>;
+  return (
+    <Suspense fallback={<Loader loading={true} />}>
+      <div className=" py-10 pb-20 w-full">{children}</div>
+    </Suspense>
+  );
 }
