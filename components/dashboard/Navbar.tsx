17b6f81acc139e071d3f85aad48fea50d65b40e6
egentinnamdi <egentinnamdi10@gmail.com>
2026-03-10 20:31:50 +0100

    style(dashboard): update navbar sheet styling and layout
    - Replace placeholder header with user profile and close button
    - Improve visual hierarchy of navigation items
    - Adjust spacing and colors for better readability

--- components/dashboard/Navbar.tsx
+++ components/dashboard/Navbar.tsx
@@ -1,5 +1,11 @@
 import Logo from "../Logo";
-import { BellIcon, ChevronDown, PanelRight, SearchIcon } from "lucide-react";
+import {
+  BellIcon,
+  ChevronDown,
+  ChevronRight,
+  PanelRight,
+  SearchIcon,
+} from "lucide-react";
 import { useIsMobile } from "@/hooks/useMobile";
 import {
   InputGroup,
@@ -20,6 +26,7 @@ import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
 import { cn } from "@/lib/utils";
 import {
   Sheet,
+  SheetClose,
   SheetContent,
   SheetFooter,
   SheetHeader,
@@ -47,17 +54,27 @@ export default function Navbar() {
             <PanelRight size={20} />
           </SheetTrigger>
           <SheetContent side="right" className="w-full justify-start!">
-            <SheetHeader>
-              <SheetTitle>Are you absolutely sure?</SheetTitle>
+            <SheetHeader className=" h-[10vh] border-b shadow-sm">
+              <SheetTitle className="flex items-center  h-full justify-between">
+                <div className="flex items-center  gap-5">
+                  <Avatar className="size-11">
+                    <AvatarImage src="https://github.com/shadcn.png" />
+                    <AvatarFallback>CN</AvatarFallback>
+                  </Avatar>
+                  <span className="text-lg">John Arthur</span>
+                </div>
+                <SheetClose>
+                  <ChevronRight />
+                </SheetClose>
+              </SheetTitle>
             </SheetHeader>
             <div className="p-5 flex flex-col gap-10">
               <div className="gap-3 flex-1 flex flex-col">
                 {navItems.map((item) => (
                   <span
                     className={cn(
-                      "text-lg font-medium p-3 pb-5 border-b",
-                      item.active &&
-                        "bg-primary/10 px-3 py-1.5 rounded-md text-black",
+                      "text-lg font-medium p-3 pb-5 border-b text-muted-foreground",
+                      item.active && "bg-primary/12 py-5 rounded-md text-black",
                     )}
                     key={item.title}
                   >
